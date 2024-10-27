af5bb6c4c8c862be715e2b2c94c960bd63c921d5
Laxmi Vaka <laxmivaka05@gmail.com>
2024-10-26 23:25:50 -0400

    Added Navigation links

---
+++ investher-ui/src/app/nav3/Forums/page.js
@@ -0,0 +1,13 @@
+// pages/nav1/Stocks.js
+import Link from "next/link";
+
+export default function Forums() {
+  return (
+    <div>
+      <h1>Forums</h1>
+      <p>Follow this</p>
+      {/* Add more content about stocks here */}
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+}
